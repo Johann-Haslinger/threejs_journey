3d1d161490900f8f3eaf1c8a97b0c72a4749960c
Johann Haslinger <johann.hsr@icloud.com>
2025-04-09 17:13:51 +0200

    finish lesson 5

--- first_three_project/script.js
+++ first_three_project/script.js
@@ -1,41 +1,34 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import gsap from "gsap";
 
 const sizes = {
   WIDTH: 800,
-  HEIGHT: 600 
-}
+  HEIGHT: 600,
+};
 
-const canvas = document.querySelector('canvas.webgl');
+const canvas = document.querySelector("canvas.webgl");
 
 const scene = new THREE.Scene();
 
-const group = new THREE.Group();
-group.position.y = 1;
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 'red' })
-)
-group.add(cube1);
-
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 'green' })
-)
-cube2.position.x = -2
-group.add(cube2);
-
-
+const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "red" }));
+scene.add(mesh);
 
 const camera = new THREE.PerspectiveCamera(75, sizes.WIDTH / sizes.HEIGHT);
 camera.position.z = 3;
-camera.lookAt(cube1.position);
+camera.lookAt(mesh.position);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
 });
 renderer.setSize(sizes.WIDTH, sizes.HEIGHT);
 
-renderer.render(scene, camera);
+gsap.to(mesh.position, { x: 2, duration: 1, delay: 1 });
+gsap.to(mesh.position, { x: 0, duration: 1, delay: 2 });
+
+const tick = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
