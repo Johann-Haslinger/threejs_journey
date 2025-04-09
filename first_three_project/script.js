495aed1dd46255402478305e50104c42c60fdddf
Johann Haslinger <johann.hsr@icloud.com>
2025-04-09 16:17:15 +0200

    finished lesson 4

--- first_three_project/script.js
+++ first_three_project/script.js
@@ -1,26 +1,41 @@
 import * as THREE from 'three';
 
+const sizes = {
+  WIDTH: 800,
+  HEIGHT: 600 
+}
+
 const canvas = document.querySelector('canvas.webgl');
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const group = new THREE.Group();
+group.position.y = 1;
+scene.add(group);
 
-const sizes = {
-  width: 800,
-  height: 600 
-}
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 'red' })
+)
+group.add(cube1);
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 'green' })
+)
+cube2.position.x = -2
+group.add(cube2);
+
+
+
+const camera = new THREE.PerspectiveCamera(75, sizes.WIDTH / sizes.HEIGHT);
 camera.position.z = 3;
+camera.lookAt(cube1.position);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 });
-renderer.setSize(sizes.width, sizes.height);
+renderer.setSize(sizes.WIDTH, sizes.HEIGHT);
 
 renderer.render(scene, camera);
